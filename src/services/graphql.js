e06822cef338d9facbc430aa76c9e99dabbb0ff9
Moshe Malawach <moshe.malawach@protonmail.com>
2021-06-26 01:27:02 +0200

    use public test server

--- src/services/graphql.js
+++ src/services/graphql.js
@@ -1,6 +1,6 @@
 import { GraphQLClient, gql } from 'graphql-request'
 
-export const endpoint = "http://localhost:8080"
+export const endpoint = "https://rayqltest.aleph.cloud/"
 
 export const client = new GraphQLClient(endpoint, {
 })
